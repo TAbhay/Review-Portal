27fc68d5a9b2eba342af2a4724565affe398bb59
Pushkar mourya <pushkar_1901me49@iitp.ac.in>
2021-09-09 09:25:01 +0530

    Header Added

---
+++ client/src/Components/Header/Header.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+import "./Header.css";
+// import { useSelector } from "react-redux";
+// import axios from "axios";
+
+export default function Header() {
+//   const auth = useSelector((state) => state.auth);
+
+//   const { user, isLogged } = auth;
+
+//   console.log(user.name);
+//   console.log(isLogged);
+
+//   const handleLogout = async () => {
+//     try {
+//       await axios.get("/user/logout");
+//       localStorage.removeItem("firstLogin");
+//       window.location.href = "/";
+//     } catch (err) {}
+//   };
+
+//   const userLink = () => {
+//     return (
+//       <li className="drop-nav">
+//         <Link to="#" className="avatar">
+//           {user.name}
+
+//           <i className="fas fa-arrow-down"></i>
+//         </Link>
+
+//         <ul className="dropdown">
+//           <li>
+//             <Link to="/profile">Profile</Link>
+//           </li>
+
+//           <li>
+//             <Link to="/" onClick={handleLogout}>
+//               Logout
+//             </Link>
+//           </li>
+//         </ul>
+//       </li>
+//     );
+//   };
+
+//   const transForm = {
+//     transform: isLogged ? "translateY(-5px)" : 0,
+//   };
+  return (
+    <header>
+      <div className="logo">
+        <h1>
+          <Link to="/">Full Auth</Link>
+        </h1>
+      </div>
+
+      <ul style={transForm}>
+        <li>
+          <Link to="/">
+            <i className="fas fa-shopping-cart"></i>Cart
+          </Link>
+        </li>
+        {/* {isLogged ? (
+          userLink()
+        ) : ( */}
+          <li>
+            <Link to="/login">
+              <i className="fas fa-user"></i> Sign in
+            </Link>
+          </li>
+        )}
+      </ul>
+    </header>
+  );
+}
