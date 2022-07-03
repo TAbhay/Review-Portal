b174d0aa4459593f7f30f3dfc593ffab4ecac587
TAbhay <tiwari.a.abhay@gmail.com>
2022-07-03 22:12:19 +0530

    minor fix

--- backend/server.js
+++ backend/server.js
@@ -47,4 +47,4 @@ app.get('*', (req, res) => {
 // }
 
 
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+app.listen(process.env.PORT, console.log(`Server is running on port ${PORT}`));
