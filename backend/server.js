dcc304509a35721e0c96664fca7e4b0c0068aa38
TAbhay <tiwari.a.abhay@gmail.com>
2022-07-04 00:03:18 +0530

    heroku

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const cors = require("cors")
+// const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const configDb = require("./config/db");
 const morgan = require("morgan")
@@ -13,7 +13,7 @@ configDb();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors({ origin: true, credentials: true }));
+// app.use(cors({ origin: true, credentials: true }));
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("tiny"));
