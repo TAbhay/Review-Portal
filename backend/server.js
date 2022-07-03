2ee33766c017cde23df6a56ddfef3214c910a458
TAbhay <tiwari.a.abhay@gmail.com>
2022-07-03 22:30:29 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -35,11 +35,10 @@ app.use('/api', require('./routes/upload'))
 
 // if(process.env.NODE_ENV === 'production'){
 
-app.use(express.static(path.join(__dirname, "client", "build")));
-
+app.use(express.static(path.resolve("./client/build")));
 
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	res.sendFile(path.resolve("./client/build/index.html"));
 });
 // }
 // else{
